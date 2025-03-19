42272c5fe8ea09c8e6b6de97262fc9b254bcc4b1
JeepalemSivaSankar <shankarjeepalem27@gmail.com>
2025-03-19 14:42:24 +0530

    Add files via upload

---
+++ task1803.py
@@ -0,0 +1,14 @@
+# num=[1,2,3,4,4,5,5,5,6,3,5]
+# ele=5
+# count=0
+# for i in range(0,len(num)):
+#     if num[i]==ele:
+#         count+=1
+# print(count)
+
+# list=[1,2,"hi","10000coders"]
+# res=[ ]
+# for i in range(0,len(list),1):
+#     res.append(list[i])
+# print(res)
+   
